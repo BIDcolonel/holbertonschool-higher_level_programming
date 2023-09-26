7dbb2e3916670ceedc2adad44a15b4356755d34c
BIDcolonel <ptitofdu89@live.fr>
2023-09-26 23:58:32 +0200

    function that prints the numbers from 1 to 100 separated by a space

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    verdict = ""
+    for index in range(1, 101):
+        if index % 3 == 0 and index % 5 == 0:
+            verdict += "FizzBuzz "
+        elif index % 3 == 0:
+            verdict += "Fizz "
+        elif index % 5 == 0:
+            verdict += "Buzz "
+        else:
+            verdict += str(index) + " "
+
+    print(verdict, end="")
