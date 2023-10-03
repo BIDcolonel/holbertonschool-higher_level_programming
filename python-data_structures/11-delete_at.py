bb933968bcba253644aa10676684eedd16f78b54
BIDcolonel <ptitofdu89@live.fr>
2023-10-04 01:02:47 +0200

    code simplification

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,9 +4,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    newList = []
-    for index in range(len(my_list)):
-        if index != idx:
-            newList.append(my_list[index])
-
-    return newList
+    del my_list[idx]
+    return my_list
