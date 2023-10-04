7d6b00dfa66bd4d185939c872b3be15a7f2d64fa
BIDcolonel <ptitofdu89@live.fr>
2023-10-04 10:57:01 +0200

    add line break

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,11 @@
 
 def square_matrix_simple(matrix=[]):
     resultMatrix = []
+
     for row in matrix:
         resultRow = []
         for value in row:
             resultRow.append(value ** 2)
         resultMatrix.append(resultRow)
+
     return resultMatrix
