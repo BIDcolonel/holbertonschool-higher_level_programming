19c03246f3ad311f9cd2ff1433e7e087ae05f41b
BIDcolonel <ptitofdu89@live.fr>
2023-10-05 14:35:13 +0200

    Correction Requirements fail

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        for i in range(x):
-            print(my_list[i], end=' ')
+        while True:
+            if count >= x:
+                break
+            print(my_list[count], end=' ')
+            count += 1
     except IndexError:
         pass
     finally:
         print()
-
-    return min(x, len(my_list))
+    return count
