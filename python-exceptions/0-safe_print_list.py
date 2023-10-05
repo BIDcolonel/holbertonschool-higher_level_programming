c52fe231eefdbac20fd9f9b66b0f10486e2cc897
BIDcolonel <ptitofdu89@live.fr>
2023-10-05 15:17:26 +0200

    correction code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    printed = 0
     try:
-        while True:
-            if count >= x:
-                break
-            print(my_list[count], end=' ')
-            count += 1
+        for index in range(x):
+            print(my_list[index], end="")
+            printed += 1
     except IndexError:
         pass
     finally:
         print()
-    return count
+    return printed
