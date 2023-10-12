6d753696697efcb04bc6073361e1dcc3da8b01fb
BIDcolonel <ptitofdu89@live.fr>
2023-10-12 13:32:07 +0200

    add other test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -67,3 +67,57 @@ class TestMaxInteger(unittest.TestCase):
         """
         test_list = [1.7, -8.2, -9.9, 0.5, 66.2]
         self.assertEqual(max_integer(test_list), 66.2)
+
+    def test_one_arg(self):
+        """
+        Test for passing one number to list
+        """
+        test_list = [1]
+        self.assertEqual(max_integer(test_list), 1)
+
+    def test_none_argument(self):
+        """
+        Test for passing None
+        """
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_list_with_none(self):
+        """
+        Test for list with None as one of its elements
+        """
+        with self.assertRaises(TypeError):
+            test_list = [1, 2, 3, 4, None]
+            max_integer(test_list)
+
+    def test_none_list(self):
+        """
+        Test for passing None as list
+        """
+        test_list = [None]
+        self.assertIsNone(max_integer(test_list), None)
+
+    def test_integers_and_strings(self):
+        """
+        Test for passing a string in the list
+        """
+        test_list = [1, 2, 3, 4, "2"]
+        with self.assertRaises(TypeError):
+            max_integer(test_list)
+
+    def test_string_numbers(self):
+        """
+        Test for passing a string of numbers in list
+        """
+        test_list = ["1234"]
+        self.assertEqual(max_integer(test_list), "1234")
+
+    def test_same_integers(self):
+        """
+        Test for passing same integers in list
+        """
+        test_list1 = [2, 2, 2, 2, 2]
+        test_list2 = [-55, -55, -55, -55]
+
+        self.assertEqual(max_integer(test_list1), 2)
+        self.assertEqual(max_integer(test_list2), -55)
