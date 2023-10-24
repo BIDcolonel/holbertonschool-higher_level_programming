76f2e9fed3d1fe1ec27747ea5a6666a86d247a82
BIDcolonel <ptitofdu89@live.fr>
2023-10-24 14:36:09 +0200

    And now, the Square! + Square size

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A representation of the Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes instances
+
+        Args:
+            size (_type_): size of this Square
+            x (int, optional): x of this Square. Defaults to 0.
+            y (int, optional): y of this Square. Defaults to 0.
+            id (_type_, optional): id of the object. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return string info of this Square"""
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.width)
+        return str_square + str_id + str_xy + str_size
+
+    @property
+    def size(self):
+        """Size of this Square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+# above this line: Task 10 and 11
