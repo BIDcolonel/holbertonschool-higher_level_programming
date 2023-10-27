a228096a7624397597951638c8cc79623b16fa07
BIDcolonel <ptitofdu89@live.fr>
2023-10-27 10:20:36 +0200

    add test for class square

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -25,7 +25,7 @@ class TestSquare(unittest.TestCase):
         '''Tests if Square inherits Base.'''
         self.assertTrue(issubclass(Square, Base))
 
-    def test_D_instantiation(self):
+    def test_C_instantiation(self):
         '''Tests instantiation.'''
         r = Square(10)
         self.assertEqual(str(type(r)), "<class 'models.square.Square'>")
@@ -64,7 +64,7 @@ class TestSquare(unittest.TestCase):
         msg = "y must be >= 0"
         self.assertEqual(str(e.exception), msg)
 
-    def test_D_instantiation_positional(self):
+    def test_C_instantiation_positional(self):
         '''Tests positional instantiation.'''
         r = Square(5, 10, 15)
         d = {'_Rectangle__height': 5, '_Rectangle__width': 5,
@@ -76,20 +76,20 @@ class TestSquare(unittest.TestCase):
              '_Rectangle__x': 10, '_Rectangle__y': 15, 'id': 20}
         self.assertEqual(r.__dict__, d)
 
-    def test_D_instantiation_keyword(self):
+    def test_C_instantiation_keyword(self):
         '''Tests positional instantiation.'''
         r = Square(100, id=421, y=99, x=101)
         d = {'_Rectangle__height': 100, '_Rectangle__width': 100,
              '_Rectangle__x': 101, '_Rectangle__y': 99, 'id': 421}
         self.assertEqual(r.__dict__, d)
 
-    def test_E_id_inherited(self):
+    def test_D_id_inherited(self):
         '''Tests if id is inherited from Base.'''
         Base._Base__nb_objects = 98
         r = Square(2)
         self.assertEqual(r.id, 99)
 
-    def test_F_properties(self):
+    def test_E_properties(self):
         '''Tests property getters/setters.'''
         r = Square(5, 9)
         r.size = 98
@@ -101,3 +101,35 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(r.size, 98)
         self.assertEqual(r.x, 102)
         self.assertEqual(r.y, 103)
+
+    def test_F_negative_size(self):
+        '''Test instantiation with a negative size.'''
+        with self.assertRaises(ValueError) as e:
+            r = Square(-5)
+        msg = "width must be > 0"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_G_negative_position(self):
+        '''Test instantiation with negative x and y values.'''
+        with self.assertRaises(ValueError) as e:
+            r = Square(5, -2, -3)
+        msg = "x must be >= 0"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_H_update_with_kwargs(self):
+        '''Test updating attributes using keyword arguments.'''
+        r = Square(5, 2, 3)
+        r.update(size=10, x=0, y=0)
+        d = {'_Rectangle__height': 10, '_Rectangle__width': 10,
+             '_Rectangle__x': 0, '_Rectangle__y': 0, 'id': 1}
+        self.assertEqual(r.__dict__, d)
+
+    def test_I_performance(self):
+        '''Test performance with a large number of instances.'''
+        import time
+        start_time = time.time()
+        for _ in range(10000):
+            r = Square(5)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        self.assertLessEqual(execution_time, 1.0)
