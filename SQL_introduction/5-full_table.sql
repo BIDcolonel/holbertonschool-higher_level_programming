84a2891f1e7e18a501a0d74668d191d7a52d5dda
BIDcolonel <ptitofdu89@live.fr>
2023-11-21 11:26:10 +0100

    correction code

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,6 @@
 -- Prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+SELECT CONCAT('Table: ', TABLE_NAME) AS 'Table',
+        CONCAT('Create Table: ', CREATE_TABLE) AS 'Create Table'
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+    AND TABLE_NAME = 'first_table';
