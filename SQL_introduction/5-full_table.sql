15e0e89a6180dfb1219c1780a0113e4908e69f9d
BIDcolonel <ptitofdu89@live.fr>
2023-11-21 11:30:41 +0100

    in-depth research to correct the code

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
 -- Prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server
-SELECT CONCAT('Table: ', TABLE_NAME) AS 'Table',
-        CONCAT('Create Table: ', CREATE_TABLE) AS 'Create Table'
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-    AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLES first_table;
