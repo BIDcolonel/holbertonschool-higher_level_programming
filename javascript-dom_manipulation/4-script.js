7b27179d77da34866579627316d47ee4a011a735
BIDcolonel <ptitofdu89@live.fr>
2024-01-29 18:51:41 +0100

    correction Lint + Task8 : Write a JavaScript script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML element with id hello.
    The translation of “hello” must be displayed in the HTML element with id hello
    Your script must work when it is imported from the <head> tag

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const addItemButton = document.getElementById('add_item');
   const myList = document.querySelector('.my_list');
 
-  addItemButton.addEventListener('click', function() {
+  addItemButton.addEventListener('click', function () {
     const newItem = document.createElement('li');
     newItem.textContent = 'Item';
     myList.appendChild(newItem);
