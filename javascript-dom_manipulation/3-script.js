bd6784b650990b92349cdd5c421a11bc2b42c1dd
BIDcolonel <ptitofdu89@live.fr>
2024-01-29 18:42:11 +0100

    Task3 : Write a JavaScript script that toggles the class of the header element when the user clicks on the tag id toggle_header:
    The header element must always have one class: red or green, never both in the same time and never empty. If the current class is red, when the user click on id toggle_header element, the class must be updated to green ; and the reverse.

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const redHeader = document.getElementById('red_header');
+  const header = document.querySelector('header');
+  const toggleHeader = document.getElementById('toggle_header');
+
+  toggleHeader.addEventListener('click', function () {
+    if (header.classList.contains('red')) {
+      header.classList.remove('red');
+      header.classList.add('green');
+    } else if (header.classList.contains('green')) {
+      header.classList.remove('green');
+      header.classList.add('red');
+    } else {
+      header.classList.add('red');
+    }
+  });
+});
